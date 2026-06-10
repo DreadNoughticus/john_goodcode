42de1fc62fcbd77c9211c682c6da46b2f17ee1fd
kl0vr <adventofcode@gmail.com>
2026-06-10 18:07:45 +1000

    feat: Added the random number generator
    Added a random number generator and prepared to create the tests and sorting function.

--- main.zig
+++ main.zig
@@ -1,9 +1,22 @@
 const std = @import("std");
 
+var prng: std.Random.DefaultPrng = .init(blk: {
+    var seed: u64 = undefined;
+    try std.posix.getrandom(std.mem.asBytes(&seed));
+    break :blk seed;
+});
+const rand = prng.random();
+
 pub fn main() !void {
     std.debug.print("Hello World!\n", .{});
 }
 
-test "test" {
+fn sortArray() {
+    std.debug.print("Coming soon", .{})
+}
+
+test "Array sorting" {
+    const randomArray: u16 = comptime std.Random.array(rand, u16)
+    sortArray()
     try std.testing.expect(true);
 }
