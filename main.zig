0c143e7b7c28a550ebd004726f38b3823fd43130
kl0vr <adventofcode@gmail.com>
2026-06-11 19:42:54 +1000

    fix: what the hellyante

--- main.zig
+++ main.zig
@@ -16,7 +16,7 @@ fn sortArray() !void {
 }
 
 test "Array sorting" {
-    const randomArray: u16 = std.Random.array(rand, u16, *usize);
+    const randomArray: u16 = std.Random.array(rand, u16, usize);
     std.debug.print(randomArray);
     try std.testing.expect(true);
 }
